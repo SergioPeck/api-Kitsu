ea51d7642b5a536ef87b33dfcfb30c4a57a9ff1f
SergioPeck <sergiopeckerle2004@gmail.com>
2025-12-10 21:18:14 -0300

    deployConfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import { MangaModule } from './mangas/mangas.module';
 import { ChaptersModule } from './chapters/chapters.module';
 import { ChapterViewsModule } from './chapter-views/chapter-views.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, MangaModule, ChaptersModule, ChapterViewsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      // SUPABASE
+      ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+
+      autoLoadEntities: true,
+
+      // Solo para desarrollo / primeras pruebas.
+      synchronize: true,
+    }),
+
+    // Tus módulos
+    UsersModule,
+    AuthModule,
+    MangaModule,
+    ChaptersModule,
+    ChapterViewsModule,
+  ],
 })
 export class AppModule {}
