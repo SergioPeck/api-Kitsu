08ac4167a1eb0ade4a379c2d8603d037dfdf7aaf
SergioPeck <sergiopeckerle2004@gmail.com>
2026-02-04 18:59:41 -0300

    enable CORS using frontend url from env

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import './firebase/firebase-admin';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
